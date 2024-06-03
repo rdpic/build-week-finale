ef2816e24d850f87ca5e967eab59ab80d17f1d93
rdpic <riccardodelpiccolo1@gmail.com>
2024-06-03 15:29:44 +0200

    Setup Progetto

---
+++ src/test/java/it/epicode/buildweekfinale/BuildweekfinaleApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.buildweekfinale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildweekfinaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
