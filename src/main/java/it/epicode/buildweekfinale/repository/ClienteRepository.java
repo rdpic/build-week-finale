9faca087c835920c1e249bf81b2b46423125a45b
gmverduci <marcelloverduci@gmail.com>
2024-06-04 17:55:52 +0200

    query cliente

--- src/main/java/it/epicode/buildweekfinale/repository/ClienteRepository.java
+++ src/main/java/it/epicode/buildweekfinale/repository/ClienteRepository.java
@@ -2,7 +2,42 @@ package it.epicode.buildweekfinale.repository;
 
 import it.epicode.buildweekfinale.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+    @Query("SELECT c FROM Cliente c ORDER BY c.ragioneSociale ASC")
+    List<Cliente> elencaClientiPerNome();
+
+    @Query("SELECT c FROM Cliente c ORDER BY c.fatturatoAnnuale DESC")
+    List<Cliente> elencaClientiPerFatturato();
+
+    @Query("SELECT c FROM Cliente c ORDER BY c.dataInserimento DESC")
+    List<Cliente> elencaClientiPerDataInserimento();
+
+    @Query("SELECT c FROM Cliente c ORDER BY c.dataUltimoContatto DESC")
+    List<Cliente> elencaClientiPerDataUltimoContatto();
+
+
+    @Query("SELECT c FROM Cliente c JOIN FETCH c.indirizzi i WHERE i.tipoIndirizzo = 'SEDE_LEGALE' ORDER BY i.comune.provincia.nome ASC")
+    List<Cliente> elencaClientiPerProvinciaSedeLegale();
+
+
+    @Query("SELECT c FROM Cliente c WHERE c.fatturatoAnnuale > :minRevenue AND c.fatturatoAnnuale < :maxRevenue")
+    List<Cliente> findByFatturatoAnnuo(@Param("minRevenue") BigDecimal minRevenue, @Param("maxRevenue") BigDecimal maxRevenue);
+
+
+    @Query("SELECT c FROM Cliente c WHERE c.dataInserimento >= :startDate")
+    List<Cliente> findByDataInserimento(@Param("startDate") LocalDate startDate);
+
+    @Query("SELECT c FROM Cliente c WHERE c.dataUltimoContatto <= :endDate")
+    List<Cliente> findByDataUltimoContatto(@Param("endDate") LocalDate endDate);
+
+    @Query("SELECT c FROM Cliente c WHERE c.ragioneSociale LIKE %:partialName%")
+    List<Cliente> findByNomeParziale(@Param("partialName") String partialName);
 }
