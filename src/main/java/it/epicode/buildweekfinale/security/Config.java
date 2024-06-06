597b7a0a07d514fdbf63959d12befb4eed7d9939
gmverduci <marcelloverduci@gmail.com>
2024-06-06 14:47:59 +0200

    mail registrazione utente

--- src/main/java/it/epicode/buildweekfinale/security/Config.java
+++ src/main/java/it/epicode/buildweekfinale/security/Config.java
@@ -1,7 +1,9 @@
 package it.epicode.buildweekfinale.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,6 +20,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
+import java.util.Properties;
 
 @Configuration
 @EnableWebSecurity
@@ -73,4 +76,32 @@ public class Config implements WebMvcConfigurer {
         return new JwtFilter();
     }
 
+
+    @Bean
+    public JavaMailSenderImpl getJavaMailSender(@Value("${gmail.mail.transport.protocol}" )String protocol,
+                                                @Value("${gmail.mail.smtp.auth}" ) String auth,
+                                                @Value("${gmail.mail.smtp.starttls.enable}" )String starttls,
+                                                @Value("${gmail.mail.debug}" )String debug,
+                                                @Value("${gmail.mail.from}" )String from,
+                                                @Value("${gmail.mail.from.password}" )String password,
+                                                @Value("${gmail.smtp.ssl.enable}" )String ssl,
+                                                @Value("${gmail.smtp.host}" )String host,
+                                                @Value("${gmail.smtp.port}" )String port){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(Integer.parseInt(port));
+
+        mailSender.setUsername(from);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+        props.put("mail.debug", debug);
+        props.put("smtp.ssl.enable",ssl);
+
+        return mailSender;
+
+    }
 }
--- src/main/java/it/epicode/buildweekfinale/service/UtenteService.java
+++ src/main/java/it/epicode/buildweekfinale/service/UtenteService.java
@@ -5,6 +5,8 @@ import it.epicode.buildweekfinale.entity.Utente;
 import it.epicode.buildweekfinale.exception.BadRequestException;
 import it.epicode.buildweekfinale.repository.UtenteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,9 @@ public class UtenteService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JavaMailSenderImpl javaMailSender;
 
     public List<Utente> getAllUtenti() {
         return utenteRepository.findAll();
@@ -42,12 +47,23 @@ public class UtenteService {
             utente.setPassword(passwordEncoder.encode(utenteDto.getPassword()));
             utente.setRuoloUtente(utenteDto.getRuoloUtente());
             utenteRepository.save(utente);
+            sendMail(utente.getEmail());
 
             return "Utente con ID " + utente.getId() + " creato con successo.";
         } else {
             throw new BadRequestException("Lo username è già stato preso.");
         }
     }
+
+    private void sendMail(String email) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("Signup");
+        message.setText("Registrazione effettuata!");
+
+        javaMailSender.send(message);
+    }
+
 
     public void deleteUtenteById(Integer id) {
         utenteRepository.deleteById(id);
